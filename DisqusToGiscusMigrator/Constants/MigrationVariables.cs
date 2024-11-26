79b8fae69c20c63598b1683c30ee7897ccc62399
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-26 18:20:01 +0800

    clean up

--- DisqusToGiscusMigrator/Constants/MigrationVariables.cs
+++ DisqusToGiscusMigrator/Constants/MigrationVariables.cs
@@ -6,9 +6,6 @@ public static class MigrationVariables
 
     public static readonly string[] IgnoredUsers =
     [
-        "disqus_9qND497TKc",
-        "matteightyate",
-        "tiagov8",
-        "disqus_GUDwEt6KYY"
+        "",
     ];
 }
