e5186e54cce5ee12a4f6856f682abab726376b83
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-27 15:12:43 +0800

    added logger

---
+++ DisqusToGiscusMigrator/Helpers/Logger.cs
@@ -0,0 +1,30 @@
+﻿namespace DisqusToGiscusMigrator.Helpers;
+
+public class Logger
+{
+    public static void Log(string message, LogLevel level)
+    {
+        Console.ForegroundColor = level switch
+        {
+            LogLevel.Info => ConsoleColor.Green,
+            LogLevel.Warning => ConsoleColor.Yellow,
+            LogLevel.Error => ConsoleColor.Red,
+            _ => Console.ForegroundColor
+        };
+
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [{level}] {message}");
+        Console.ResetColor();
+    }
+
+    public static void Log(string method)
+    {
+        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] Running {method} ...");
+    }
+}
+
+public enum LogLevel
+{
+    Info,
+    Warning,
+    Error
+}
