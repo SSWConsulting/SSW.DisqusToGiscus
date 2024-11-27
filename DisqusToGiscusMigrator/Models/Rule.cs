44d0a5c3a80a669d5fe56715ec4d0c9563498aec
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-27 15:19:52 +0800

    added Rule model, updated other models

---
+++ DisqusToGiscusMigrator/Models/Rule.cs
@@ -0,0 +1,8 @@
+﻿namespace DisqusToGiscusMigrator.Models;
+
+public class Rule
+{
+    public string File { get; set; } = string.Empty;
+
+    public string Guid { get; set; } = string.Empty;
+}
--- DisqusToGiscusMigrator/Models/RulesHistory.cs
+++ DisqusToGiscusMigrator/Models/RulesHistory.cs
@@ -6,8 +6,10 @@ public class RulesHistory
 {
     [JsonPropertyName("file")]
     public string File { get; set; } = string.Empty;
+
     [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
+
     [JsonPropertyName("uri")]
     public string Uri { get; set; } = string.Empty;
 }
